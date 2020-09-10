a6b05757fa4a8b05df2fae39d164c8787c00aae9
mei38 <70788879+mei38@users.noreply.github.com>
2020-09-10 15:19:56 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -12,7 +12,7 @@ jsproxy_config({
     'defalut': {
       label: '第1通道',
       lines: {
-        '111.111.workers.dev': 1,
+        'cold-hill-db83.calvinamnwomsr.workers.dev': 1,
       }
     },
     
@@ -20,7 +20,7 @@ jsproxy_config({
     'defalut1': {
       label: '第2通道',
       lines: {
-        '222.222.workers.dev': 1,
+        'white-recipe-fa43.calvinamnwomsr.workers.dev': 1,
       }
     },
     
